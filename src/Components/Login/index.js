35731e5192412c7faf534767b9497ef4b624601a
Anmol varma <anmol_varma@github.com>
2018-03-06 23:05:23 +0530

    Login api called on button click

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -11,38 +11,54 @@ class Login extends React.Component {
       password: '',
     };
   }
-  updateUsername(event) {
-    if (event.target.value.match(/^[a-zA-Z0-9_.-]+$/)) {
-      this.setState({
-        username: event.target.value,
-      });
-    }
-  }
 
-  makeLoginRequest() {
-    axios.post('/login', {
-      userName: this.state.password,
-      password: this.state.username,
-    }).then((response) => {
-      console.log(response)
-        .catch((err) => {
-          console.log(err.message);
-        });
-    });
-  }
   render() {
+    const makeLoginRequest = () => {
+      axios.post('/login', {
+        userName: this.state.username,
+        password: this.state.password,
+      }).then((response) => {
+        console.log(response);
+      }).catch((err) => {
+        console.log('error', err.message);
+      });
+    };
+    const updateUsername = (event) => {
+      if (event.target.value.match(/^[a-zA-Z0-9_.-]*$/)) {
+        this.setState({
+          username: event.target.value,
+        });
+      }
+    };
+
+    const updatePassword = (event) => {
+      this.setState({
+        password: event.target.value,
+      });
+    };
     return (
       <div className="Login-Container">
         <div className="Login-box">
           <h2 className="Login-heading">Login</h2>
           <div className="Login-fields">
-            <input className="Login-input-field" type="text" value={this.state.username} placeholder="Username" />
+            <input
+              className="Login-input-field"
+              type="text"
+              onChange={updateUsername}
+              value={this.state.username}
+              placeholder="Username"
+            />
           </div>
           <div className="Login-fields">
-            <input className="Login-input-field" type="password" placeholder="Password" />
+            <input
+              onChange={updatePassword}
+              className="Login-input-field"
+              type="password"
+              placeholder="Password"
+            />
           </div>
           <div className="Login-fields">
-            <button onClick={this.makeLoginRequest} className="Login-button">
+            <button onClick={makeLoginRequest} className="Login-button">
               <span className="Login-button-label">Login</span>
             </button>
           </div>
