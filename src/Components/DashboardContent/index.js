72c48ef6649c062dafcc2b2997e462b06a77272a
Anmol varma <anmol_varma@github.com>
2018-03-11 20:22:17 +0530

    Transfer money component

--- src/Components/DashboardContent/index.js
+++ src/Components/DashboardContent/index.js
@@ -33,6 +33,18 @@ const miniStatementButton = () => (
   </div>
 );
 
+const sendMoneyButtonInBody = () => (
+  <div className="Dashboardcontent-body-transfer-button-wrapper">
+    <button
+      className="Dashboardcontent-body-transfer-button"
+    >
+      <span className="Dashboardcontent-body-transfer-button-label">
+    Transfer money
+      </span>
+    </button>
+  </div>
+);
+
 const amountField = () => (
   <div className="Dashboardcontent-header-field">
     <input type="number" placeholder="Amount" className="Dashboardcontent-header-input-field" />
@@ -67,7 +79,22 @@ const DashboardContent = props => (
         </div>
       </div>
       <div className="Dashboardcontent-body">
-        <div className="Dashboardcontent-body-transfer-form">Anmol</div>
+        <div className="Dashboardcontent-body-transfer-form">
+          <div className="Dashboardcontent-body-form-transfer-form">
+            <div className="Dashboardcontent-body-form-heading">
+              <div className="Dashboardcontent-body-form-heading-label">Send money instantly</div>
+            </div>
+            <div className="Dashboardcontent-body-form-username">
+              {usernameField()}
+            </div>
+            <div className="Dashboardcontent-body-form-amount">
+              {amountField()}
+            </div>
+            <div className="Dashboardcontent-body-form-button">
+              {sendMoneyButtonInBody()}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
