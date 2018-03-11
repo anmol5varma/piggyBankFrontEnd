91668651a4637537c7e1cb7becd775d03195e46a
Anmol varma <anmol_varma@github.com>
2018-03-12 02:22:09 +0530

    Main class for dashboard content and onClick Component changed

--- src/Components/DashboardContent/index.js
+++ src/Components/DashboardContent/index.js
@@ -4,59 +4,74 @@ import './Dashboardcontent.css';
 import DashboardContentBody from '../DashboardContentBody';
 import MiniStatement from '../MiniStatement';
 
-const usernameField = () => (
-  <div className="Dashboardcontent-header-field">
-    <input type="text" placeholder="Username" className="Dashboardcontent-header-input-field" />
-  </div>
-);
+// const usernameField = () => (
+//   <div className="Dashboardcontent-header-field">
+//     <input type="text" placeholder="Username" className="Dashboardcontent-header-input-field" />
+//   </div>
+// );
 
-const sendMoneyButton = () => (
-  <div className="Dashboardcontent-header-transfer-button-wrapper">
-    <button
-      className="Dashboardcontent-header-transfer-button"
-    >
-      <span className="Dashboardcontent-header-transfer-button-label">
-    Transfer money
-      </span>
-    </button>
-  </div>
-);
+// const sendMoneyButtonInBody = () => (
+//   <div className="Dashboardcontent-body-transfer-button-wrapper">
+//     <button
+//       className="Dashboardcontent-body-transfer-button"
+//     >
+//       <span className="Dashboardcontent-body-transfer-button-label">
+//     Transfer money
+//       </span>
+//     </button>
+//   </div>
+// );
+//
+// const amountField = () => (
+//   <div className="Dashboardcontent-header-field">
+//     <input type="number" placeholder="Amount" className="Dashboardcontent-header-input-field" />
+//   </div>
+// );
 
-const miniStatementButton = () => (
-  <div className="Dashboardcontent-header-transfer-button-wrapper">
-    <button
-      className="Dashboardcontent-header-transfer-button"
-    >
-      <span className="Dashboardcontent-header-transfer-button-label">
-    Account summary
-      </span>
-    </button>
-  </div>
-);
+class DashboardContent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showComponent: 1,
+    };
+  }
+  render() {
+    const sendMoneyButton = () => (
+      <div className="Dashboardcontent-header-transfer-button-wrapper">
+        <button
+          onClick={() => {
+            this.setState({
+              showComponent: 2,
+            });
+          }}
+          className="Dashboardcontent-header-transfer-button"
+        >
+          <span className="Dashboardcontent-header-transfer-button-label">
+        Transfer money
+          </span>
+        </button>
+      </div>
+    );
 
-const sendMoneyButtonInBody = () => (
-  <div className="Dashboardcontent-body-transfer-button-wrapper">
-    <button
-      className="Dashboardcontent-body-transfer-button"
-    >
-      <span className="Dashboardcontent-body-transfer-button-label">
-    Transfer money
-      </span>
-    </button>
-  </div>
-);
+    const miniStatementButton = () => (
+      <div className="Dashboardcontent-header-transfer-button-wrapper">
+        <button
+          onClick={() => {
+            this.setState({
+              showComponent: 1,
+            });
+          }}
+          className="Dashboardcontent-header-transfer-button"
+        >
+          <span className="Dashboardcontent-header-transfer-button-label">
+        Account summary
+          </span>
+        </button>
+      </div>
+    );
 
-const amountField = () => (
-  <div className="Dashboardcontent-header-field">
-    <input type="number" placeholder="Amount" className="Dashboardcontent-header-input-field" />
-  </div>
-);
-
-const DashboardContent = props => (
-  <div className="Dashboardcontent-container">
-    <div className="Dashboardcontent-playcard">
+    const header = props => (
       <div className="Dashboardcontent-header">
-
         <div className="Dashboardcontent-header-wrapper">
           <div className="Dashboardcontent-header-balance">
             <div className="Dashboardcontent-header-wallet">
@@ -73,23 +88,38 @@ const DashboardContent = props => (
             </div>
           </div>
         </div>
-
         <div className="Dashboardcontent-header-wrapper">
           <div className="Dashboardcontent-header-transfer-button-container">
             {sendMoneyButton()}
           </div>
         </div>
-
         <div className="Dashboardcontent-header-wrapper">
           <div className="Dashboardcontent-header-transfer-button-container">
             {miniStatementButton()}
           </div>
         </div>
       </div>
-      <MiniStatement />
-    </div>
-  </div>
-);
+    );
+    if (this.state.showComponent === 1) {
+      return (
+        <div className="Dashboardcontent-container">
+          <div className="Dashboardcontent-playcard">
+            {header(this.props)}
+            <MiniStatement />
+          </div>
+        </div>
+      );
+    }
+    return (
+      <div className="Dashboardcontent-container">
+        <div className="Dashboardcontent-playcard">
+          {header(this.props)}
+          <DashboardContentBody />
+        </div>
+      </div>
+    );
+  }
+}
 
 DashboardContent.propTypes = {
   balance: PropTypes.string.isRequired,
--- src/Components/MiniStatement/index.js
+++ src/Components/MiniStatement/index.js
@@ -1,43 +1,66 @@
 import React from 'react';
 import './Ministatement.css';
 
-const allRecords = () => (
-  <div className="Ministatement-allRecords">
-    <div className="Ministatement-each-row Ministatement-allRecord-heading">
-      <div className="Ministatement-record-sno Ministatement-record-heading">
-        <div className="Ministatement-record-head">S.No.</div>
-      </div>
-      <div className="Ministatement-record-name Ministatement-record-heading">
-        <div className="Ministatement-record-head">Name</div>
-      </div>
-      <div className="Ministatement-record-amount Ministatement-record-heading">
-        <div className="Ministatement-record-head">Amount</div>
-      </div>
-      <div className="Ministatement-record-balance Ministatement-record-heading">
-        <div className="Ministatement-record-head">Balance</div>
-      </div>
-      <div className="Ministatement-record-type Ministatement-record-heading">
-        <div className="Ministatement-record-head">Type</div>
+const eachRecord = () => (
+  <div className="Ministatement-each-row Ministatement-allRecord-entry">
+    <div className="Ministatement-record-value Ministatement-record-sno">
+      <div className="Ministatement-record-entry">a</div>
+    </div>
+    <div className="Ministatement-record-value Ministatement-record-name">
+      <div className="Ministatement-record-entry">b</div>
+    </div>
+    <div className="Ministatement-record-value Ministatement-record-amount">
+      <div className="Ministatement-record-entry">
+        <i className="fas fa-rupee-sign" /> 100
       </div>
     </div>
-    <div className="Ministatement-each-row">
-      <div className="Ministatement-record-value Ministatement-record-sno">a</div>
-      <div className="Ministatement-record-value Ministatement-record-name">b</div>
-      <div className="Ministatement-record-value Ministatement-record-amount">c</div>
-      <div className="Ministatement-record-value Ministatement-record-balance">d</div>
-      <div className="Ministatement-record-value Ministatement-record-type">e</div>
+    <div className="Ministatement-record-value Ministatement-record-balance">
+      <div className="Ministatement-record-entry">
+        <i className="fas fa-rupee-sign" /> 100
+      </div>
     </div>
-    <div className="Ministatement-each-row">
-      <div className="Ministatement-record-value Ministatement-record-">a</div>
-      <div className="Ministatement-record-value Ministatement-record-">b</div>
-      <div className="Ministatement-record-value Ministatement-record-">c</div>
-      <div className="Ministatement-record-value Ministatement-record-">d</div>
-      <div className="Ministatement-record-value Ministatement-record-type">e</div>
+    <div className="Ministatement-record-value Ministatement-record-type">
+      <div className="Ministatement-record-entry">e</div>
     </div>
   </div>
 );
 
-const MiniStatement = props => (
+const recordHeading = () => (
+  <div className="Ministatement-each-row Ministatement-allRecord-heading">
+    <div className="Ministatement-record-heading Ministatement-record-sno">
+      <div className="Ministatement-record-head">S.No.</div>
+    </div>
+    <div className="Ministatement-record-heading Ministatement-record-name">
+      <div className="Ministatement-record-head">Receipent</div>
+    </div>
+    <div className="Ministatement-record-heading Ministatement-record-amount">
+      <div className="Ministatement-record-head">Amount</div>
+    </div>
+    <div className="Ministatement-record-heading Ministatement-record-balance">
+      <div className="Ministatement-record-head">Balance</div>
+    </div>
+    <div className="Ministatement-record-heading Ministatement-record-type">
+      <div className="Ministatement-record-head">Type</div>
+    </div>
+  </div>
+);
+
+const allRecords = () => (
+  <div className="Ministatement-allRecords">
+    {recordHeading()}
+    {eachRecord()}
+    {eachRecord()}
+    {eachRecord()}
+    {eachRecord()}
+    {eachRecord()}
+    {eachRecord()}
+    {eachRecord()}
+    {eachRecord()}
+
+  </div>
+);
+
+const MiniStatement = () => (
   <div className="Ministatement-container">
     <div className="Ministatement-table-wrapper">
       <div className="Ministatement-content">
