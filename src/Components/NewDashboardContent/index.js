e9f44c9e3112a82faa88d7a03597ba98e998cfcd
Anmol varma <anmol_varma@github.com>
2018-03-13 20:43:43 +0530

    New header with transfer embedded

---
+++ src/Components/NewDashboardContent/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './Newdashboardcontent.css';
+
+class DashboardContent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      balance: '10000',
+    };
+  }
+  render() {
+    const inputField = (onChangeFunction, inputType, placeholder) => (
+      <div className="Dashboardcontent-header-transfer-box-inputfield">
+        <input
+          type={inputType}
+          placeholder={placeholder}
+          onChange={onChangeFunction}
+        />
+      </div>
+    );
+
+    return (
+      <div className="Dashboardcontent-container">
+        <div className="Dashboardcontent-wrapper">
+          <div className="Dashboardcontent-username">Hello Anmol</div>
+          <div className="Dashboardcontent-header">
+            <div className="Dashboardcontent-header-balance">
+              <div>
+                <i className="material-icons Dashboardcontent-header-wallet-icon">
+        account_balance_wallet
+                </i>
+              </div>
+              <div className="Dashboardcontent-header-balance-label">
+                <div className="Dashboardcontent-header-balance-title">Balance</div>
+                <div className="Dashboardcontent-header-balance-value">
+                  <i className="fas fa-rupee-sign Dashboardcontent-header-balance-icon" />
+                  <span className=" Dashboardcontent-header-balance-amount">
+                    {this.state.balance}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="Dashboardcontent-header-transfer">
+              <div className="Dashboardcontent-header-transfer-box">
+                <div className="Dashboardcontent-header-transfer-box-title">transfer money</div>
+                <div className="Dashboardcontent-header-transfer-box-inputs">
+                  {inputField(null, 'text', 'Username')}
+                  {inputField(null, 'number', 'Amount')}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="Dashboardcontent-statement">Minisatetement</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DashboardContent;
