04580b78e9bd20775e0fe93583e17de74fe0e5d9
surabhi8 <surabhigupta2442.com>
2018-03-23 18:43:56 +0530

    Minor ui fixes

--- src/Components/SignupSide/index.js
+++ src/Components/SignupSide/index.js
@@ -9,7 +9,7 @@ class SignUpSide extends React.Component {
       <div className="Signupside-container">
         <div className="Signupside-color-div">
           <div className="Signupside-box">
-            <div className="Signupside-logo">Piggy Bank</div>
+            <div className="Signupside-logo">DigiBank</div>
             <div className="Signupside-welcome-message">
               <div className="Signupside-heading">Welcome</div>
               <div className="Signupside-subbheading">
@@ -17,15 +17,7 @@ class SignUpSide extends React.Component {
               </div>
               <div className="Signupside-content">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua. Neque convallis a cras semper. Sollicitudin ac orci
-                phasellus egestas tellus. Vel facilisis volutpat est velit egestas
-                dui id ornare arcu. Adipiscing tristique risus nec feugiat in.
-                Platea dictumst quisque sagittis purus sit amet. Nulla facilisi
-                etiam dignissim diam quis. Quam viverra orci sagittis eu volutpat
-                odio facilisis mauris. Facilisi cras fermentum odio eu feugiat pretium.
-                Elementum nibh tellus molestie nunc. Tempor orci eu lobortis elementum
-                nibh tellus molestie nunc.
+                sed do dhgf dhjfgejh dfjhgsdjf
               </div>
               <div className="Signupside-button-wrapper">
 
