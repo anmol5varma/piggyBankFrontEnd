9e0bb865975c89726ca7582de0b2c6e2258a519f
paridhi <paridhi815@gmail.com>
2018-03-26 21:43:56 +0530

    Basic component for forgot password added

--- src/Components/ForgotPassword/index.js
+++ src/Components/ForgotPassword/index.js
@@ -10,17 +10,82 @@ class ResetPassword extends React.Component {
   constructor() {
     super();
     this.state = {
+      noOfComponents: 0,
+      userName: '',
     };
-  }
-  componentDidMount() {
-    Axios.post('/forgot/password', { userName: 'Anmol' });
   }
 
 
   render() {
+    const updateUserName = (event) => {
+      this.setState({
+        userName: event.target.value,
+      });
+    };
+    const changeComponent = () => {
+      this.setState({
+        noOfComponents: 1,
+      });
+    };
+    const onForgotPassword = () => {
+      Axios.post('/forgot/password', { username: this.state.userName }).then(() => {
+        changeComponent();
+        // console.log('Password Reset successfull');
+      });
+    };
+    if (this.state.noOfComponents === 0) {
+      return (
+        <div>
+          <div className="username-container">
+            <TransparentInputField
+              className="change-password-input-field-container"
+              type="text"
+              placeholder="Enter your userName"
+              value={this.state.userName}
+              onChange={updateUserName}
+            />
+          </div>
+          <div className="reset-password-button-container" >
+            <button
+              className="SignupContent-button"
+              onClick={onForgotPassword}
+              disabled={this.state.disabled}
+            >
+
+              <span className="SignupContent-button-label">
+                      Send E-Mail with Reset Password Link
+              </span>
+            </button>
+          </div>
+        </div>
+      );
+    }
     return (
-      <div className="change-password-container" >
-           An email has been sent to your Registered Email Id.Please visit your inbox to reset your password.
+      <div>
+        <div className="username-container">
+          <TransparentInputField
+            className="change-password-input-field-container"
+            type="text"
+            placeholder="Enter your userName"
+            value={this.state.userName}
+            onChange={updateUserName}
+          />
+        </div>
+        <div className="reset-password-button-container" >
+          <button
+            className="SignupContent-button"
+            onClick={onForgotPassword}
+            disabled={this.state.disabled}
+          >
+
+            <span className="SignupContent-button-label">
+                      Send E-Mail with Reset Password Link
+            </span>
+          </button>
+        </div>
+        <div className="change-password-container" >
+         An email has been sent to your Registered Email Id.Please visit your inbox to reset your password.
+        </div>
       </div>
     );
   }
