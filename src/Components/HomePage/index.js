634eef4c79c3a490fe8e32d9f41cfd7fdaa30304
Anmol varma <anmol_varma@github.com>
2018-03-28 19:48:01 +0530

    Google translate icon removed

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Homepage.css';
 import LoginSide from '../LoginSide';
 import SignupSide from '../SignupSide';
+import Language from '../Language';
 import PropTypes from 'prop-types';
 
 class HomePage extends React.Component {
@@ -9,6 +10,7 @@ class HomePage extends React.Component {
     const { history } = this.props;
     return (
       <div className="HomePage-Container">
+        <Language />
         <div className="HomePage-signup"><SignupSide history={history} /></div>
         <div className="HomePage-login"><LoginSide history={history} /></div>
       </div>
---
+++ src/Components/Language/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './language.css';
+
+
+class Language extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+  render() {
+    return (
+      <div id="google_translate_element" />
+    );
+  }
+}
+
+export default Language;
