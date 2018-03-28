699e031a1a6d28df36627f8695396c12e7cf9772
paridhi <paridhi815@gmail.com>
2018-03-28 10:25:29 +0530

    Forgot Password with New Component Working

--- src/Components/PasswordForgotForm/index.js
+++ src/Components/PasswordForgotForm/index.js
@@ -1,27 +1,21 @@
-import axios from 'axios';
+
+import { withAlert } from 'react-alert';
 import { Link } from 'react-router-dom';
 import React from 'react';
 import './PasswordForgotForm.css';
 
-const form = (updateUsername, updatePassword, incorrectPasswordError, incorrectUsernameError) => (
+const Axios = require('axios');
+
+const form = (updateUserName, incorrectUsernameError) => (
   <div className="cont">
     <input
       type="text"
-      placeholder="Username"
-      className="Login-input-field"
-      onChange={updateUsername}
+      placeholder="UserName"
+      className="PasswordForgotForm-input-field"
+      // value={this.state.userName}
+      onChange={updateUserName}
     />
     <div className="Error-message">{incorrectUsernameError}</div>
-    <input
-      type="password"
-      placeholder="Password"
-      className="Login-input-field"
-      onChange={updatePassword}
-    />
-    <div className="Error-message">{incorrectPasswordError}</div>
-    <div className="Forgot-Password">
-      <Link to="/forgotPassword"><a>Forgot password?</a></Link>
-    </div>
   </div>
 );
 
@@ -29,51 +23,48 @@ class PasswordForgotForm extends React.Component {
   constructor() {
     super();
     this.state = {
-      username: '',
-      password: '',
-      incorrectPasswordError: '',
+      userName: '',
       incorrectUsernameError: '',
     };
   }
   render() {
-    const makeLoginRequest = () => {
-      axios.post('/login', {
-        userName: this.state.username,
-        password: this.state.password,
-      }).then((response) => {
-        if (window.localStorage) {
-          localStorage.setItem('token', JSON.stringify({ token: response.headers.token }));
-        }
-        this.props.history.push('/user');
-      }).catch((err) => {
-        if (err.response.data.message === 'Please check password') {
-          this.setState({
-            incorrectPasswordError: 'Please enter correct password',
-          });
-        } else if (err.response.data.message === 'Please check user name') {
-          this.setState({ incorrectUsernameError: 'Incorrect Username' });
-        } else {
-          this.setState({ incorrectUsernameError: 'Invaid Username' });
-        }
+    const onForgotPassword = () => {
+      Axios.post('/forgot/password', { username: this.state.userName }).then(() => {
+        const message = 'An email has been sent to your Registered Email Id.Please visit your inbox to reset your password.';
+        const promise = Promise.resolve(message);
+        promise.then((message1) => {
+          this.props.alert.success(message1, { onClose: () => this.props.history.push('/login') });
+        });
       });
+      // axios.post('/login', {
+      //   userName: this.state.username,
+      //   password: this.state.password,
+      // }).then((response) => {
+      //   if (window.localStorage) {
+      //     localStorage.setItem('token', JSON.stringify({ token: response.headers.token }));
+      //   }
+      //   this.props.history.push('/user');
+      // }).catch((err) => {
+      //   if (err.response.data.message === 'Please check password') {
+      //     this.setState({
+      //       incorrectPasswordError: 'Please enter correct password',
+      //     });
+      //   } else if (err.response.data.message === 'Please check user name') {
+      //     this.setState({ incorrectUsernameError: 'Incorrect Username' });
+      //   } else {
+      //     this.setState({ incorrectUsernameError: 'Invaid Username' });
+      //   }
+      // });
     };
-    const updateUsername = (event) => {
+    const updateUserName = (event) => {
       if (event.target.value.match(/^[a-zA-Z0-9_.-]*$/)) {
         this.setState({
-          username: event.target.value,
-          incorrectPasswordError: '',
+          userName: event.target.value,
           incorrectUsernameError: '',
         });
       }
     };
 
-    const updatePassword = (event) => {
-      this.setState({
-        password: event.target.value,
-        incorrectPasswordError: '',
-        incorrectUsernameError: '',
-      });
-    };
     return (
       <div className="PasswordForgotForm-container">
         <div className="PasswordForgotForm-box">
@@ -81,16 +72,14 @@ class PasswordForgotForm extends React.Component {
             <div className="PasswordForgotForm-heading">Your User Name</div>
             <div className="PasswordForgotForm-content">
               {form(
-               updateUsername,
-                updatePassword,
-                this.state.incorrectPasswordError,
+               updateUserName,
                 this.state.incorrectUsernameError,
               )}
             </div>
             <div className="PasswordForgotForm-button-wrapper">
-              <button className="PasswordForgotForm-button" onClick={() => makeLoginRequest()}>
+              <button className="PasswordForgotForm-button" onClick={() => onForgotPassword()}>
                 <span className="PasswordForgotForm-button-label">
-          login
+                Send E-Mail
                 </span>
               </button>
             </div>
@@ -100,4 +89,4 @@ class PasswordForgotForm extends React.Component {
     );
   }
 }
-export default PasswordForgotForm;
+export default withAlert(PasswordForgotForm);
--- src/Components/Render/index.js
+++ src/Components/Render/index.js
@@ -12,7 +12,7 @@ import ResetPassword from '../ResetPassword';
 
 // import history from '../../history';
 const options = {
-  timeout: 4000,
+  timeout: 10000,
   position: 'top center',
   offset: '10px',
   transition: 'scale',
